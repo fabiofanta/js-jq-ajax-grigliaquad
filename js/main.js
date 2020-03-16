9aba89b98cde54c4c133092ec9bdb69bc9c2b6b2
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-03-16 17:31:33 +0100

    3-rel

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 // Griglia 6x6, ad ogni click parte una richiesta AJAX che prende un numero random da 1 a 9. Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde. Il numero ottenuto appare al centro del quadrato.
 
 $(document).ready(function() {
+
+    //handlebars template
+    var source = $('#square-template').html();
+    var template = Handlebars.compile(source);
+    var html = template();
+    //end
+
+    // build box by creating 36 squares in '.box' div
+    boxCreator('.box',36,html);
+
+
     $('.square').click(function() {
         var self = this
         $.ajax({
@@ -9,26 +20,28 @@ $(document).ready(function() {
             success : function(data) {
                 var randomNumber = data.response;
                 console.log(randomNumber);
-                test(randomNumber,self);
+                randomSq(randomNumber,self);
             } ,
             error : function() {
                 console.log('Non funziona!');
             }
         });
     })
-
-
-
-    function test(randN,selector) {
-        $(selector).text(randN);
-        $(selector).removeClass('yellow green');
-        if (randN <= 5) {
-            $(selector).toggleClass('yellow');
-        } else {
-            $(selector).toggleClass('green');
-        }
-    }
-
-
-
 });
+
+
+function randomSq(randN,selector) {
+    $(selector).text(randN);
+    $(selector).removeClass('yellow green');
+    if (randN <= 5) {
+        $(selector).toggleClass('yellow');
+    } else {
+        $(selector).toggleClass('green');
+    }
+};
+
+function boxCreator(selector,sqNumbers,template) {
+    for (var i = 0; i < sqNumbers; i++) {
+        $(selector).append(template);
+    };
+}
